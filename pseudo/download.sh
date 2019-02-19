29f143f12390e0f44dfba13243657208be43dff5
Glenn Hickey <glenn.hickey@gmail.com>
2019-02-19 16:17:10 -0500

    remove

--- pseudo/download.sh
+++
@@ -1,9 +0,0 @@
-aws s3 cp s3://majorsv-ucsc/gt/sv_vcf/reduced.tab .
-aws s3 cp s3://majorsv-ucsc/gt/sv_vcf/variants_reduced.vcf.gz.tbi .
-aws s3 cp s3://majorsv-ucsc/gt/sv_vcf/variants_reduced.vcf.gz .
-
-vt rminfo variants_reduced.vcf.gz -t END -o pseudo_diploid.vcf
-bgzip -f pseudo_diploid.vcf
-tabix -f -p vcf pseudo_diploid.vcf.gz
-
-
